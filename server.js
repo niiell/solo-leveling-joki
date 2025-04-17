c772881a7a90fe641afbcb79754664895c9b9b01
niiell <niellsamosir@gmail.com>
2025-04-18 06:57:23 +0700

    Update server.js to disable index file serving in public directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 // Route to serve the landing page
 app.get('/', (req, res) => {
